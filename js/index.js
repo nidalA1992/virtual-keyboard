4fdf619aa0e97b2599c49bfb3481b1d9adba3718
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-04-27 15:59:13 +0500

    feat: add key layout schema

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
+import {Button} from "./Button.js";
+import {keys} from "./keys.js";
+
 const app = document.getElementById('app');
+
+// keys.forEach(i => {
+//   i.en.forEach(i => new Button(i).attach(app));
+//   app.append(document.createElement('br'));
+// });
---
+++ js/keys.js
@@ -0,0 +1,33 @@
+export const keys = [
+  {
+    en: ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+    shiftEn: ['~', '!', '@', '#', '$', '%', '^', '&', '(', ')', '_', '+', 'Backspace'],
+    ru: ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+    shiftRu: ['Ё', '!', '"', '№', ';', '%', ':', '?', '(', ')', '_', '+', 'Backspace'],
+  },
+  {
+    en: ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
+    shiftEn: ['Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|'],
+    ru: ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\'],
+    shiftRu: ['Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/'],
+  },
+  {
+    en: ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
+    shiftEn: ['CapsLock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', 'Enter'],
+    ru: ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
+    shiftRu: ['CapsLock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter'],
+  },
+  {
+    en: ['ShiftLeft', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'ShiftRight', 'ArrowUp'],
+    shiftEn: ['ShiftLeft', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'ShiftRight', 'ArrowUp'],
+    ru: ['ShiftLeft', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'ShiftRight', 'ArrowUp'],
+    shiftRu: ['ShiftLeft', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', 'ShiftRight', 'ArrowUp'],
+  },
+  {
+    en: ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
+    shiftEn: ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
+    ru: ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
+    shiftRu: ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
+  },
+
+]
