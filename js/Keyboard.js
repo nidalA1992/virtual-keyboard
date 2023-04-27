5548188fe9b04632fad42415d359e56f18a16d61
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-04-27 23:35:33 +0500

    feat: add field keys in keyboard class, for storage buttons instance

--- js/Keyboard.js
+++ js/Keyboard.js
@@ -2,12 +2,17 @@ import {keys} from "./keys.js";
 import {Button} from "./Button.js";
 
 export class Keyboard {
-  constructor() {}
+  constructor() {
+    this.keys = {};
+  }
 
   init(container) {
 
     keys.forEach(i => {
-      i.en.forEach(i => new Button(i).init(container));
+      i.en.forEach(i => {
+        this.keys[i] = new Button(i);
+        this.keys[i].init(container);
+      });
 
       container.append(document.createElement('br'));
     });
