1e2a52c90def91f017ff087f7d4601b7b2643fcc
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-04-27 23:50:02 +0500

    refactor: delete getbutton function

--- js/utils.js
+++
@@ -1,7 +0,0 @@
-export function getButton(e) {
-
-  //проверка является ли нажатая клавиша символьным элементом
-  const code = e.key.length > 1 || e.key === ' ' ? e.code : e.key;
-  return keyboard.keys[code];
-
-}
