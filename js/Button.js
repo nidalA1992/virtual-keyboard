82146bb6320675c3d7acf65e5e308882d9aad201
nidalA1992 <nidalakhmedjanov@gmail.com>
2023-04-27 16:55:58 +0500

    feat: add active and deactive method to Button class

--- js/Button.js
+++ js/Button.js
@@ -1,11 +1,25 @@
 export class Button {
   constructor(value) {
     this.button = document.createElement('button');
+    this.button.setAttribute('data-key', value);
     this.button.textContent = value;
+
+    this.active = this.active.bind(this);
+    this.deactive = this.deactive.bind(this);
   }
 
-  attach (container) {
+  active() {
+    this.button.classList.add('active');
+  }
+
+  deactive() {
+    this.button.classList.remove('active');
+  }
+
+  init(container) {
     container.append(this.button);
-  }
 
+    this.button.addEventListener('mousedown', this.active);
+    this.button.addEventListener('mouseup', this.deactive);
+  }
 }
